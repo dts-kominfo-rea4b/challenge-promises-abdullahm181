4f585013349eb3cbe97a02cfdcd8196416c2eedf
abdullahm181 <muhammadhunter2@gmail.com>
2022-09-18 14:38:11 +0700

    Modified promise.js with the task given

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+//Punya Muhammad Amin Abdullah
+const promiseOutput = async (emosi) =>{
+  try{
+    var JumlahMarah=0,JumlahTidakMarah=0;
+    const XXI= await promiseTheaterIXX();
+    const VGC= await promiseTheaterVGC();
+    XXI.forEach(Sum);
+    VGC.forEach(Sum);
+    function Sum(item) {
+      if(item.hasil=="marah")
+        JumlahMarah++;
+      if(item.hasil=="tidak marah")
+        JumlahTidakMarah++;
+    }
+    if (emosi=="marah")
+      console.log(JumlahMarah);
+    if (emosi=="tidak marah")
+      console.log(JumlahTidakMarah);
+  }catch(rejectMessage){
+    console.log(rejectMessage);
+  }
+
+}
+
 
 module.exports = {
   promiseOutput,
